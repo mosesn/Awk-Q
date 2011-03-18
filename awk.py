0da462552eacc5bd5b46797b9814cd4008195da3
Moses <nnythm@gmail.com>
2011-03-18 12:43:54 -0400

    needs design now, mostly done

--- awk.py
+++ awk.py
@@ -8,17 +8,16 @@ class Wobsite:
         self.db = Redis()
 
     def index(self):
-        #should give user a random string
-        #there should also be a link to a submit page
-#        self.db.
-        return str(Template(file="views/index.tmpl",searchList = [{"question":"Cool."}]))
+        return str(Template(file="views/index.tmpl",searchList = [{"question":self.db.srandmember("awk_qs")}]))
     index.exposed = True
 
     def submit(self, awk_q = None):
+        bool = False
         if awk_q:
             self.db.sadd("new_subs",awk_q)
+            bool = True
         #should let the user post a random string
-        return "Submit page."
+        return str(Template(file="views/submit.tmpl",searchList = [{"submitted":bool}]))
     submit.exposed = True
 
     def set_cookie(self, username = None, password = None):
@@ -26,18 +25,29 @@ class Wobsite:
             cookie = cherrypy.response.cookie
             cookie['username'] = username
             cookie['username']['max-age'] = 3600
+            cookie['password'] = hashlib.sha224(password).hexdigest()
+            cookie['password']['max-age'] = 3600
             return str(Template(file="views/set_cookie.tmpl",searchList = [{"good":True}]))
         else:
             return str(Template(file="views/set_cookie.tmpl",searchList = [{"good":False}]))
 
     set_cookie.exposed = True
 
-    def approve(self):
+    def approve(self,which = None, del_which = None):
         cookie = cherrypy.request.cookie
-        if cookie['password'].value == self.db.get(cookie['username'].value):
-            return "Big success!"
+        if cookie and cookie['password'].value == self.db.get(cookie['username'].value):
+            bool = False
+            if del_which:
+                bool = True
+                self.db.srem("new_subs",del_which)
+            elif which:
+                bool = True
+                self.db.sadd("awk_qs",which)
+                self.db.srem("new_subs",which)
+                
+            return str(Template(file="views/approve.tmpl",searchList = [{"submits":self.db.smembers("new_subs"),"changed":bool,"approved":True}]))
         else:
-            return "Oh no . . ."
+            return str(Template(file="views/approve.tmpl",searchList = [{"submits":[],"changed":False,"approved":False}]))
     approve.exposed = True
 
     def get_random(self):
